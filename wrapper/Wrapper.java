8f590973bd5d578eeeba32ad42ab7b967d450c29
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-11-04 16:58:35 -0300

    add wrapperclass

---
+++ wrapper/Wrapper.java
@@ -0,0 +1,12 @@
+package wrapper;
+
+public class Wrapper {
+    int tipoInt = 1;
+    float tipoFloat = 1;
+    boolean tipoBoolean = false;
+
+//    int - char
+    Float tipoFloat2 = 1;
+    Boolean tipoBoolean2 = false;
+    Integer tipoInt2 = 1;
+}
